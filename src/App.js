18cc3f6c3162e7d902ef1317bf7a4831d38038d8
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-08-27 18:42:22 +0200

    mkl

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      name: { FirstName: 'Metin', LastName: 'Mogensen' }
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>Hi {this.state.name.FirstName + ' ' + this.state.name.LastName}   </p>
+          <button
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  name: { FirstName: 'Melisa', LastName: 'Mogenseen' }
+                }
+              },
+                () => {
+                  console.log(this.state)
+                }
+              )
+            }}
           >
-            Learn React
-          </a>
+            Change name </button>
+
         </header>
       </div>
     );
