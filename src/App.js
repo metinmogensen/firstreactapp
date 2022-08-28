878245a50aee5ee3eb6f649e35435af7f2b0cff6
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-08-28 19:49:30 +0200

    read
    Made component map (same search functionality) next push will be with a search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -11,23 +11,16 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    console.log('constructor');
   }
 
   //lifecycle methode 
   componentDidMount() {
-    console.log('componentDidMount');
 
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((Response) => Response.json())
       .then((users) => this.setState(() => {
         return { monsters: users }
-      },
-        () => {
-          console.log(this.state)
-        }))
-
-
+      }))
   }
 
   onSearchChange = (event) => {
@@ -39,7 +32,6 @@ class App extends Component {
   }
 
   render() {
-    console.log('render');
 
     // resturcturerer da deet ser bedre ud en random variable
     const { monsters, searchField } = this.state;
@@ -51,22 +43,13 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box'
+        <input
+          className='search-box'
           type='search'
           placeholder='Search Monsters'
           onChange={onSearchChange} />
 
-        {
-          results.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-          })
-        }
-
-
+        <CardList monsters={results} />
       </div>
     );
   }
